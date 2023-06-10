e8eef02769cf462d327a8121b3ce00c69253ccb6
Hoang Dinh <hoang.dv@outlook.com>
2023-06-10 23:44:05 +0900

    Add `query` function to base repository

--- src/dynamodb/repositories/base.repository.ts
+++ src/dynamodb/repositories/base.repository.ts
@@ -1,8 +1,33 @@
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  QueryCommand,
+  QueryCommandInput,
+} from '@aws-sdk/lib-dynamodb';
 import { Injectable } from '@nestjs/common';
 
+type ArrayElement<T> = T extends Array<infer R> ? R : never;
+
+type QueryInput<TModel, TModelKey = keyof TModel> = Omit<
+  QueryCommandInput,
+  'TableName' | 'ProjectionExpression' | 'ExpressionAttributeNames'
+> & {
+  ProjectionExpression?: Array<TModelKey>;
+  ExpressionAttributeNames?: Partial<
+    Record<`#${TModelKey & string}`, TModelKey & string>
+  >;
+};
+
+type QueryOutput<TModel, TParams> = TParams extends {
+  ProjectionExpression: infer TProjection;
+}
+  ? TProjection extends Array<keyof TModel>
+    ? Array<Pick<TModel, Extract<ArrayElement<TProjection>, keyof TModel>>>
+    : never
+  : TModel[];
+
 @Injectable()
-export abstract class BaseRepository<T extends object> {
+export abstract class BaseRepository<T> {
   protected abstract tableName: string;
 
   constructor(private readonly docClient: DynamoDBDocumentClient) {}
@@ -16,5 +41,47 @@ export abstract class BaseRepository<T extends object> {
     );
 
     return Item ? (Item as T) : null;
+  }
+
+  async query<TParams extends QueryInput<T>>(
+    params: TParams,
+  ): Promise<QueryOutput<T, TParams>> {
+    const result: any[] = [];
+
+    const queryParams: QueryCommandInput = {
+      ...params,
+      TableName: this.tableName,
+      ExpressionAttributeNames: params.ExpressionAttributeNames as Record<
+        string,
+        string
+      >,
+      ProjectionExpression: undefined,
+    };
+
+    if (params.ProjectionExpression) {
+      queryParams.ProjectionExpression = params.ProjectionExpression.map(
+        (p) => `#${String(p)}`,
+      ).join(',');
+      queryParams.ExpressionAttributeNames = {
+        ...params.ProjectionExpression.reduce((acc, p) => {
+          acc[`#${String(p)}`] = String(p);
+          return acc;
+        }, {} as Record<string, string>),
+        ...(queryParams.ExpressionAttributeNames ?? {}),
+      };
+    }
+
+    do {
+      const { Items, LastEvaluatedKey } = await this.docClient.send(
+        new QueryCommand(queryParams),
+      );
+      if (Items) {
+        result.push(...Items);
+      }
+
+      queryParams.ExclusiveStartKey = LastEvaluatedKey;
+    } while (queryParams.ExclusiveStartKey);
+
+    return result as QueryOutput<T, TParams>;
   }
 }
